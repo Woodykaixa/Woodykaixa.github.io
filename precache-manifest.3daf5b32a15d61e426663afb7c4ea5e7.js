f8ab75fd4031d67f811435227a8f8dd6416cf394
kaixa <690750353@qq.com>
2020-09-09 18:31:30 +0800

    使用react构建页面

---
+++ precache-manifest.3daf5b32a15d61e426663afb7c4ea5e7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49d2b0482ccabdf210091e7b5f7d8a19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5c2c8d2fb1ae07b851e7",
+    "url": "/static/css/2.8117de4d.chunk.css"
+  },
+  {
+    "revision": "d6d49d3e15685e7e9e78",
+    "url": "/static/css/main.84a27ed6.chunk.css"
+  },
+  {
+    "revision": "5c2c8d2fb1ae07b851e7",
+    "url": "/static/js/2.7c73ce1b.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/static/js/2.7c73ce1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d6d49d3e15685e7e9e78",
+    "url": "/static/js/main.998a7977.chunk.js"
+  },
+  {
+    "revision": "5cd1569e6b561c89928b",
+    "url": "/static/js/runtime-main.b96e7864.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
