a61907b3fa72329d4070beb1b1f37dc8f9791248
kaixa <690750353@qq.com>
2020-09-10 00:08:10 +0800

    添加可读文档列表

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3daf5b32a15d61e426663afb7c4ea5e7.js"
+  "/precache-manifest.7b489586cd692b30975f6edaf93f361d.js"
 );
 
 self.addEventListener('message', (event) => {
