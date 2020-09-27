680c854660fd7f479c58bfceaf8485a51c9d0f9a
kaixa <690750353@qq.com>
2020-09-27 16:52:21 +0800

    sasuga me

---
+++ precache-manifest.5930408f772ac8a74079c7af339e5918.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8adb7554e49a69ddb47b3e0fe52937b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7b670c66d05d0121b8d",
+    "url": "/static/css/2.8117de4d.chunk.css"
+  },
+  {
+    "revision": "70b5099872b2fc3b8119",
+    "url": "/static/css/main.2d9d0c49.chunk.css"
+  },
+  {
+    "revision": "d7b670c66d05d0121b8d",
+    "url": "/static/js/2.39f28916.chunk.js"
+  },
+  {
+    "revision": "92bcffc488d6f3fbd12a7720ee4a3b28",
+    "url": "/static/js/2.39f28916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70b5099872b2fc3b8119",
+    "url": "/static/js/main.5d19a31e.chunk.js"
+  },
+  {
+    "revision": "5cd1569e6b561c89928b",
+    "url": "/static/js/runtime-main.b96e7864.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.7b489586cd692b30975f6edaf93f361d.js"
+  "/precache-manifest.5930408f772ac8a74079c7af339e5918.js"
 );
 
 self.addEventListener('message', (event) => {
